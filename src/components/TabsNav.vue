563717e01ff6df20395dcbdf02ba6684fa6c9c6f
amr <amrmounir2@gmail.com>
2023-05-15 18:26:34 +0300

    first commit

--- src/components/TabsNav.vue
+++ src/components/TabsNav.vue
@@ -1,31 +1,32 @@
 <template>
-    <ion-footer>
-            <ion-toolbar>
-    <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="home" />
-          <ion-label>Home</ion-label>
-        </ion-tab-button>
-        <!-- <ion-tab-button tab="tab2" href="/tabs/tab2">
+  <ion-footer>
+    <ion-toolbar>
+      <ion-tabs>
+        <ion-router-outlet></ion-router-outlet>
+        <ion-tab-bar slot="bottom">
+          <ion-tab-button tab="tab1" href="/tabs/tab1">
+            <ion-icon :icon="home" />
+            <ion-label>Home</ion-label>
+          </ion-tab-button>
+          <!-- <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon :icon="statsChart" />
           <ion-label>Discount</ion-label>
         </ion-tab-button> -->
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="settings" />
-          <ion-label>Settings</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
-</ion-toolbar>
-        </ion-footer>
+          <ion-tab-button tab="tab3" href="/tabs/tab3">
+            <ion-icon :icon="settings" />
+            <ion-label>Settings</ion-label>
+          </ion-tab-button>
+        </ion-tab-bar>
+      </ion-tabs>
+    </ion-toolbar>
+  </ion-footer>
 </template>
 
 <script setup>
-import { IonTabBar, IonTabButton, IonTabs, 
-    IonLabel, IonIcon, IonRouterOutlet, 
-    IonFooter, IonToolbar
+import {
+  IonTabBar, IonTabButton, IonTabs,
+  IonLabel, IonIcon, IonRouterOutlet,
+  IonFooter, IonToolbar
 } from '@ionic/vue';
 import { home, settings } from 'ionicons/icons';
 
--- src/services/common/requestMaker.js
+++ src/services/common/requestMaker.js
@@ -1,63 +1,61 @@
 // Request gateway file
 
 import axios from "axios";
-import TokenService from "../../utils/TokenService"
+import TokenService from "../../utils/TokenService";
 
-
-const ENDPOINT = process.env.VUE_APP_ROOT_API;
-/* const ENDPOINT = "https://nccbuddy.pythonanywhere.com/" */
+// const ENDPOINT = process.env.VUE_APP_ROOT_API;
+const ENDPOINT = "https://nccbuddy.pythonanywhere.com/";
 
 export default class RequestMaker {
+  async POST(data, endpoint) {
+    let URL = ENDPOINT.concat(endpoint);
+    let response_data = null;
+    let error_data = null;
+    let headers = null;
 
-    async POST(data, endpoint) {
-        let URL = ENDPOINT.concat(endpoint);
-        let response_data = null
-        let error_data = null
-        let headers = null
-        
-        // get token to pass in header
-        const token = await TokenService.getToken()
-        if (token !== null) {
-            headers = {"headers":{"Authorization": "Token ".concat(token)}}
-        }
-
-        try {
-            await axios.post(URL, data, headers).then(
-                response => (response_data = response.data)
-            ).catch(
-                err => { error_data = err.response.data }
-            )
-            return response_data ? response_data : error_data
-        } catch (error) {
-            console.log(error);
-        }
+    // get token to pass in header
+    const token = await TokenService.getToken();
+    if (token !== null) {
+      headers = { headers: { Authorization: "Token ".concat(token) } };
     }
 
-    async GET(endpoint, params) {
-        let URL = ENDPOINT.concat(endpoint);
-        let response_data = null
-        let error_data = null
-        let config = {}
-
-        // get token to pass in header
-        const token = await TokenService.getToken()
-        if (token !== null) {
-            config = {"headers":{"Authorization": "Token ".concat(token)}}
-        }
-        if (params) {
-            config["params"] = params
-        }
-        try {
-            await axios.get(URL, config).then(
-                response => (response_data = response.data)
-            ).catch(
-                err => { error_data = err.response.data }
-            )
-            return response_data ? response_data : error_data
-        } catch (error) {
-            console.log(error);
-        }
-        
+    try {
+      await axios
+        .post(URL, data, headers)
+        .then((response) => (response_data = response.data))
+        .catch((err) => {
+          error_data = err.response.data;
+        });
+      return response_data ? response_data : error_data;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
+  async GET(endpoint, params) {
+    let URL = ENDPOINT.concat(endpoint);
+    let response_data = null;
+    let error_data = null;
+    let config = {};
+
+    // get token to pass in header
+    const token = await TokenService.getToken();
+    if (token !== null) {
+      config = { headers: { Authorization: "Token ".concat(token) } };
+    }
+    if (params) {
+      config["params"] = params;
+    }
+    try {
+      await axios
+        .get(URL, config)
+        .then((response) => (response_data = response.data))
+        .catch((err) => {
+          error_data = err.response.data;
+        });
+      return response_data ? response_data : error_data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
